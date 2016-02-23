8de9032df2aaeabdf17761f74795a07d1027256a
ivandd <ivandd@users.noreply.github.com>
2016-02-23 13:30:07 -0800

    Initial Commit

---
+++ ImageAppOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageAppOne
+//
+//  Created by Ivan Davchev on 2/23/16.
+//  Copyright © 2016 Ivan Davchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
