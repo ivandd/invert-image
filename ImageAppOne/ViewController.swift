1029f7581b0592842ecdd571b55d64c3a9a92b0e
ivandd <ivandd@users.noreply.github.com>
2016-02-23 23:57:28 -0800

    Checkpoint

--- ImageAppOne/ViewController.swift
+++ ImageAppOne/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  ImageAppOne
+//  Invert Image
 //
 //  Created by Ivan Davchev on 2/23/16.
 //  Copyright © 2016 Ivan Davchev. All rights reserved.
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var imageViewController: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // @IBOutlet weak var imageViewControllerz: UIImageView!
     
     @IBOutlet weak var saveNewImageButton: UIBarButtonItem!
+    
+    @IBOutlet weak var imageConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewTop: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintRight: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewBottom: NSLayoutConstraint!
+    
+    var lastZoomScale: CGFloat = -1
     
     let imagePicker = UIImagePickerController()
     
@@ -29,9 +42,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let selectorAsString = "imageWasSavedSuccessfully:didFinishSavingWithError:context:"
         let selectorToCall = Selector(selectorAsString)
         
-        if (imageViewController.image != nil) {
+        if (imageView.image != nil) {
             UIImageWriteToSavedPhotosAlbum(
-                imageViewController.image!,
+                imageView.image!,
                 self,
                 selectorToCall,
                 nil)
@@ -61,10 +74,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     func invertImage() {
-        let inputImage = CIImage(image: imageViewController.image!)
+        let inputImage = CIImage(image: imageView.image!)
         let filteredImage = inputImage!.imageByApplyingFilter("CIColorInvert", withInputParameters: nil)
         let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
-        imageViewController.image = UIImage(CGImage: renderedImage)
+        imageView.image = UIImage(CGImage: renderedImage)
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
@@ -76,11 +89,94 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         didFinishPickingMediaWithInfo info: [String : AnyObject])
     {
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
-        imageViewController.contentMode = .ScaleAspectFit //3
-        imageViewController.image = chosenImage //4
+        // imageView.contentMode = .ScaleAspectFit //3
+        imageView.image = chosenImage //4
+        print ("Picked an image")
         dismissViewControllerAnimated(true, completion: nil) //5
         invertImage()
+        print ("Inverted the image")
         saveNewImageButton.enabled = true
+    }
+
+    
+    func updateConstraints() {
+        if let image = imageView.image {
+            let imageWidth = image.size.width
+            let imageHeight = image.size.height
+            
+            let viewWidth = scrollView.bounds.size.width
+            let viewHeight = scrollView.bounds.size.height
+
+            print ("scrollViewTop = \(scrollViewTop.constant)")
+            print ("scrollViewBottom = \(scrollViewBottom.constant)")
+            
+            // center image if it is smaller than the scroll view
+            var hPadding = (viewWidth - scrollView.zoomScale * imageWidth) / 2
+            if hPadding < 0 { hPadding = 0 }
+            print ("hPadding = \(hPadding)")
+            
+            var vPadding = (viewHeight - scrollView.zoomScale * imageHeight) / 2
+            if vPadding < 0 { vPadding = 0 }
+            print ("vPadding = \(vPadding)")
+            
+            imageConstraintLeft.constant = hPadding
+            imageConstraintRight.constant = hPadding
+            
+            print ("scrollViewTop = \(scrollViewTop.constant)")
+            print ("scrollViewBottom = \(scrollViewBottom.constant)")
+            
+            imageConstraintTop.constant = vPadding
+            imageConstraintBottom.constant = vPadding
+
+            view.layoutIfNeeded()
+        
+            print ("Updated image constraints")
+        }
+    }
+    
+    // Zoom to show as much image as possible unless image is smaller than the scroll view
+    private func updateZoom() {
+        if let image = imageView.image {
+            print (scrollView.bounds.size.width)
+            print (image.size.width)
+            print (scrollView.bounds.size.height)
+            print (image.size.height)
+            
+            var minZoom = min(scrollView.bounds.size.width / image.size.width,
+                scrollView.bounds.size.height / image.size.height)
+            print ("minZoom = \(minZoom)")
+            
+            if minZoom > 1 { minZoom = 1 }
+            
+            scrollView.minimumZoomScale = minZoom
+            
+            // Force scrollViewDidZoom fire if zoom did not change
+            if minZoom == lastZoomScale { minZoom += 0.000001 }
+            print ("minZoom = \(minZoom)")
+            
+            minZoom = 1
+            
+            scrollView.zoomScale = minZoom
+            lastZoomScale = minZoom
+            print ("Updated zoom to \(minZoom)")
+        }
+    }
+
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // imageView.image = UIImage(named: imageScrollLargeImageName)
+        scrollView.delegate = self
+        updateZoom()
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateConstraints()
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 
 }
