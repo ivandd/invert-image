d724571cce10a2be01b06c00a5068af3e4f3235e
ivandd <ivandd@users.noreply.github.com>
2016-02-23 17:34:39 -0800

    First decent version of the app

--- ImageAppOne/ViewController.swift
+++ ImageAppOne/ViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageViewController: UIImageView!
+    
+    @IBOutlet weak var saveNewImageButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
+    // Create place to render
+    let context = CIContext(options: nil)
+    
+    @IBAction func openImage(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveNewImage(sender: AnyObject) {
+        let selectorAsString = "imageWasSavedSuccessfully:didFinishSavingWithError:context:"
+        let selectorToCall = Selector(selectorAsString)
+        
+        if (imageViewController.image != nil) {
+            UIImageWriteToSavedPhotosAlbum(
+                imageViewController.image!,
+                self,
+                selectorToCall,
+                nil)
+        }
+    }
+    
+    func imageWasSavedSuccessfully(image: UIImage,
+        didFinishSavingWithError error: NSError!,
+        context: UnsafeMutablePointer<()>) {
+            
+        if let theError = error {
+            print("An error happened while saving the image = \(theError)")
+        } else {
+            print("Image was saved successfully")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +60,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func invertImage() {
+        let inputImage = CIImage(image: imageViewController.image!)
+        let filteredImage = inputImage!.imageByApplyingFilter("CIColorInvert", withInputParameters: nil)
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
+        imageViewController.image = UIImage(CGImage: renderedImage)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(
+        picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        imageViewController.contentMode = .ScaleAspectFit //3
+        imageViewController.image = chosenImage //4
+        dismissViewControllerAnimated(true, completion: nil) //5
+        invertImage()
+        saveNewImageButton.enabled = true
+    }
 
 }
 
